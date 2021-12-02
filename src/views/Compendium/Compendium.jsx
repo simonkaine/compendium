9d71bcfd25763025b3e52d9bfd7dcd005e417d43
simonkaine <simonkaine@gmail.com>
2021-12-02 10:44:46 -0800

    React app up and running | Work In Progress.

---
+++ src/views/Compendium/Compendium.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { useEffect } from 'react';
+
+export default function Compendium() {
+    // const [loadingState, setLoadingState] = useEffect(true);
+    // const [pokeDetails, setPokeDetails] = useEffect('');
+    // const [pokeTypes, setPokeTypes] = useEffect('');
+    // const [selected, setSelectedPokemon] = useEffect('')
+
+    return (
+        <div>
+            <h1>Working?</h1>
+        </div>
+    )
+}
+
